da484a0e6a51d34e1901126c856ec42e4efd9092
Sean Bain <artemis45566@gmail.com>
2024-01-28 06:33:56 +0000

    Finished again

--- script2.js
+++ script2.js
@@ -1,61 +1,72 @@
 const ctx = myCanvas.getContext("2d");
 
-
-const centerX = myCanvas.width / 2;
-const centerY = myCanvas.height / 2;
-const radius = 50;
-const left = centerX - radius;
-const rectTop = centerY - radius;
-
-ctx.beginPath();
-ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-ctx.clip();
+myCanvas.width = 500;
+myCanvas.height = 500;
 
 
-
-// left rectangle
-
-ctx.fillStyle = "white";
-ctx.fillRect(
-    left,
-    rectTop,
-    radius,
-    radius * 2
-);
-
-
-// right rectangle
-
-ctx.fillStyle = "black";
-ctx.fillRect(
-    centerX, 
-    rectTop,
-    radius,
-    radius * 2
+drawYinYang(
+    myCanvas.width / 2,
+    myCanvas.height / 2,
+    200
 )
 
 
-// top circle
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(centerX, centerY-(radius/2), radius/2, 0, Math.PI*2);
-ctx.fill();
+function drawYinYang(centerX, centerY, radius) {
 
-// black dot
-ctx.fillStyle = "black";
-ctx.beginPath();
-ctx.arc(centerX, centerY-(radius/2), 10, 0, Math.PI*2);
-ctx.fill();
+    const left = centerX - radius;
+    const rectTop = centerY - radius;
+
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.clip();
 
 
-// bottom circle
-ctx.beginPath();
-ctx.arc(centerX, centerY+(radius/2), radius/2, 0, Math.PI*2);
-ctx.fill();
+
+    // left rectangle
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(
+        left,
+        rectTop,
+        radius,
+        radius * 2
+    );
 
 
-// white dot
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(centerX, centerY+(radius/2), 10, 0, Math.PI*2);
-ctx.fill();
+    // right rectangle
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(
+        centerX, 
+        rectTop,
+        radius,
+        radius * 2
+    )
+
+
+    // top circle
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(centerX, centerY-(radius/2), radius/2, 0, Math.PI*2);
+    ctx.fill();
+
+    // black dot
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(centerX, centerY-(radius/2), radius/5, 0, Math.PI*2);
+    ctx.fill();
+
+
+    // bottom circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY+(radius/2), radius/2, 0, Math.PI*2);
+    ctx.fill();
+
+
+    // white dot
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(centerX, centerY+(radius/2), radius/5, 0, Math.PI*2);
+    ctx.fill();
+
+}
